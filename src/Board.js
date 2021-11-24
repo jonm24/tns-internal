50d3cdfc47a2e9a8c1b73934be2e5135169beaa0
Jonathan Michalak <themichalakfeed@gmail.com>
2021-11-24 01:18:25 -0500

    fix filters

--- src/Board.js
+++ src/Board.js
@@ -1,24 +1,27 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import Event from './Event';
 
 export default function Board ({ onlyDMs, onlyMentions, data }) {
+  const [filtered, setFiltered] = useState(data);
+
+  useEffect(() => setFiltered( 
+      onlyDMs && onlyMentions ?
+        data.sort((a, b) => new Date(b.time) - new Date(a.time))
+            .filter(obj => (obj.event.includes("direct_message_events") || obj.event.includes("tweet_create_events")))
+      : onlyDMs ? 
+        data.sort((a, b) => new Date(b.time) - new Date(a.time))
+            .filter(obj => obj.event.includes("direct_message_events")) 
+      : onlyMentions ? 
+        data.sort((a, b) => new Date(b.time) - new Date(a.time))
+            .filter(obj => obj.event.includes("tweet_create_events"))
+      : 
+        data.sort((a, b) => new Date(b.time) - new Date(a.time))
+    ), [onlyDMs, onlyMentions, data])
+
   return (
     <Fragment>
-      { 
-        onlyDMs && onlyMentions // both filters
-        ? data.sort((a, b) => new Date(b.time) - new Date(a.time)).filter(obj => Object.keys(obj).includes("direct_message_events") || Object.keys(obj).includes("tweet_create_events")).map((item, index) => (
-          <Event key={index} event={JSON.stringify(item)}/>)) 
-        
-        : onlyDMs // only direct messages
-        ? data.sort((a, b) => new Date(b.time) - new Date(a.time)).filter(obj => Object.keys(obj).includes("direct_message_events")).map((item, index) => (
-          <Event key={index} event={JSON.stringify(item)}/>))
-
-        : onlyMentions // only mentions
-        ? data.sort((a, b) => new Date(b.time) - new Date(a.time)).filter(obj => Object.keys(obj).includes("tweet_create_events")).map((item, index) => (
-          <Event key={index} event={JSON.stringify(item)}/>))
-          
-        : data.sort((a, b) => new Date(b.time) - new Date(a.time)).map((item, index) => ( // all events
-        <Event key={index} event={JSON.stringify(item)}/>))
+      {
+        filtered.map((item, index) => (<Event key={index} event={JSON.stringify(item)}/>))
       }
     </Fragment>
   )
